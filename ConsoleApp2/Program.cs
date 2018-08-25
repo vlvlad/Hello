2041a3d4f5ace96411194fb3acf5fa4791912065
Vasiliy Naboikin <steveperson666@gmail.com>
2018-08-25 15:34:31 +0300

    Console.WriteLine("o");

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -15,6 +15,7 @@ namespace ConsoleApp2
             Console.WriteLine("W");
             Console.WriteLine("R");
             Console.WriteLine("p");
+            Console.WriteLine("o");
         }
     }
 }
